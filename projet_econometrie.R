1711aa3a50e1eee3929452509e173ecaa3f3a2be
clement2323 <guillo.clement@gmail.com>
2019-04-19 18:24:06 +0200

    sélection de variables pertinentes et filtrage sur les actifs occués

--- projet_econometrie.R
+++ projet_econometrie.R
@@ -1,50 +1,171 @@
 rm(list=ls())
 gc()
 library(data.table)
-library(foreign)
-library(readstata13)
-library(dplyr)
+# library(foreign)
+ library(readstata13)
+# library(dplyr)
 
 
-path = "C:/Users/Hugues/Desktop/Cours Ensae/econo/nouvelles_donnees"
+
+#########################################
+####Liste des des variables par thème####
+#########################################
+ 
+
+####champ de l'étude#### L'individu est délimité par ident noi
+# 
+# act #activité au sens du bit# actif occupe 1 oui 2 non
+# stat2 # statut de travail, saarie non slari, ce sont les salariés qui  nous intéressent
+# stc #statu de la profession principale 1  son compte ou salarié chef d'entp, salarié(autre que ch d'entp) 3 taf pour sa famille
+# actop# 1 oui non
+# ident #identifiant anoymisé du logement
+# noi # numéro individuel
+# noech #numéro de l'échantillon
+# noicon #numéro individuel du conjoint
+# annee # annee de l'enquete
+# trim # trimestre de l'enquete
+# 
+# 
+# ####type de travail####
+# 
+# contrat #type de contrat de travail CDI CDD
+# ancentr #(et ancentr4) ancienneté de l'entreprise en mois
+# art #travail dans le milieu artistique  ?
+# dchant#durée du chomage précédent l'emploi (peut expliquer unsaliaire faible si élevé)
+# dudet #durée du contrat de travail (joue sur le salaire ?)
+# efen# effectif de l'entreprise
+# efet # effectif de l'établissement
+# naf # activité économique de l'emploi occupé
+# titc#type de fonctionnaire  eleve, titulaire ou contractuel
+# 
+# 
+# ####Socio démographie####
+# ag # age detaié année révolu
+# typmen #type de ménage
+# typmen15 #idem 15 positions
+# nbactoc # nombre actifs occupés dans le logement
+# nbagenf # nombre et age des enfants
+# nat28 #nationalite
+# naia #année de naissance
+# naim # mois de naissance
+# cohab # vie en couple ?
+# matri # statut matrimonial légal
+# cse #CSP des actifs
+# csei #idem non détaillé 
+# cspp #csp pere
+# cspm #csp mere
+# csptot # csp(pas forcément pour les actifs)
+# sexe # sexe
+# scj# sexe du conjoint
+# so # statut d'occupation du logement
+# 
+# ###Localisation
+# depeta #  ?
+# deparc #code département de l'établissem
+# reg #région de résidence
+# 
+# 
+# ####Salaire####
+# prim #complément de primes dans le salaire
+# revent #tranche de revenu annuel
+# salmee # salaire déclaré de l'emploi principal
+# salmet #tranche de salaire déclaré de l'emploi principal
+# salred # salaire mensuel et redressé des non réponses
+# 
+# 
+# ####Horaires et temps de travail####
+# 
+# sousempl#situation de sous emploi sans obj si ts complet ou inactif
+# nbheur#nombre d'heures correspondant au salaire déclaré
+# nbhp #nombre d'heures normalement prévues par semaines
+# hhc#nombre moyen d'heures par semaine dans l'emploi principal
+# hhc6#idem 6 postes
+# empnh # nb d'h"ueres sup
+# emphre #nb d'heures sup rémunérées
+# jourtr #nombre moyen de jours travaillés pendant la semaine
+# tpp #temps de travail dans l'emploi principal, 1 temps complet 2 temps partiel
+# 
+# 
+# ####Diplome####
+# spe # specialite du diplome le plus élevé
+# nivet# niveau d'étude le pus élevé
+# ngen #niveau d'étude le plus élevé
+# cite97 # niveau d'éducation le plus élevé != diplome
+# datdip #n'existe pas en 2003
+# datgen # date du diplome dans l'enseignement généal et secondaire
+# datsup # date diplome dans l'enseignement supérierur
+# ddipl#diplome le plus élevé obtenu en 7 postes
+# dip # idem 16 postes
+# dip11 # idem 11 postes
+# formoi # mois de fin des études initiales
+# fordat # année de fin des études initiales
+# forsg #spécialité suivie dans l'ennseignement secondaire
+
+
+####On commence
+###on charge les bases, on filtre sur les colonnes et les lignes
+
+#ordonnées par thèmes et ordre alphaetique dans haque thèmes
+variable<-c("actop","annee","ident","noi","noicon","stat2","stc","trim",
+            "ancentr","art","contra","dchant","dudet","efen","efet","naf","titc","trefen","trefet",
+            "ag","cohab","cse","csei","cspm","cspp","cstot","matri","naia","naim","nat14","nat28","nbactoc","nbageenfa","nbagenf","scj","sexe","so","typmen5","typmen7","typmen21","typmen15",
+            "deparc","depeta","reg",
+            "prim","revent","salmee","salmet","salred",
+            "emphre","emphnh","hhc","hhc6","jourtr","nbheur","nbhp","sousempl","tpp",
+            "cite97","datdip","datgen","datsup","ddipl","dip","dip11","fordat","formoi","forsg","ngen","nivet","spe")
+
+
+path = "C:/Users/Clement/Desktop/Projet Économétrie 2/Données"
 files = list.files(path)
-variables = c("annee","trim","ag","salmee","salmet","datdip","datgen","datsup","ddipl","deparc","depeta")
+# variables = c("annee","trim","ag","salmee","salmet","datdip","datgen","datsup","ddipl","deparc","depeta")
 
-##############################################
-fichier = list()
-i = 1
-for (f in files){
-  print(f)
+
+#Pour l'instant jej dégage la grosse table des vieilles années trop lourdes.
+fichier<-lapply(files[-1],function(f){
+  #f<-files[length(files)]
   data = read.dta13(paste0(path,"/",f))
-  print(setdiff(variables,colnames(data)))
-  interet = data[,variables[variables %in% colnames(data)]]
-  fichier[[i]] = interet
-  i = i + 1
-}
+  dif<-setdiff(variable,colnames(data))
+  print(dif)
+  #equivalences de noms si 2013 et 2014
+  if(f %in% c("individu 2013.dta","individu 2014.dta")){
+    tmp<-colnames(data)
+    tmp[tmp=="dchantj"]<-"dchant" 
+    tmp[tmp=="coured"]<-"cohab"
+    colnames(data)<-tmp
+    }
+  
+  
+  #ok je filtre sur les variables et salarié et actif occupé 
+  interet = data[(data$stat2 == "2" & data$actop =="1") ,variable[variable %in% colnames(data)]]
+  
+  })
 
-?rbindlist
-fichier
+
 table_finale = rbindlist(fichier, use.names = TRUE, fill = TRUE)
 fwrite(table_finale,paste0(path,"/variables_interet.csv"))
+rm(files,path,variable,fichier)
+
+save.image(file=paste0(path,"/table_finale.Rdata"))
 
 ##############################################
 
-df = fread("C:/Users/Hugues/Desktop/Cours Ensae/econo/variables_interet.csv")
+df = fread("C:/Users/Clement/Desktop/Projet Économétrie 2/Données/variables_interet.csv")
 
-df$salmee = as.integer(df$salmee)
+# sum(is.na(df$ddipl)) #213 valeurs manquantes, on les enlève ?
 df = df[!is.na(df$ddipl)]
-# ~ 1 million de lignes 
+# ~ 440 000 lignes
+summary(df$salmee)
+summary(df$salred)
+
+
+sum(!is.na(df$salmee) | (!is.na(df$salmet) & df$salmet != ""))
 df = df[!is.na(df$salmee) | (!is.na(df$salmet) & df$salmet != "")]
 # ~ 160 000 lignes (aucun intérêt à garder lignes ou on a ni salaire ni tranche)
+#Ok mais peut-on étudier la non réponse et est-elle liée au salaire justement ? => modèle de censure  =( durr...)
+
 df[is.na(df$salmee)]
 df
 
-# experience potentielle :
-df$datsup = as.integer(df$datsup)
-df$datgen = as.integer(df$datgen)
-df$annee = as.integer(df$annee)
-df$ag = as.integer(df$ag)
-df$datdip = as.integer(df$datdip)
 # diplome :
 #- Diplôme non déclaré
 #1 - Diplôme supérieur
